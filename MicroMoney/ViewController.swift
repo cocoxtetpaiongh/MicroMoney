d2e50067a4ca2dfd9de581383ece04fe007dd4d6
cocoxtetpai <cocoxtetpai@gmail.com>
2017-12-02 09:36:59 +0630

    Initial Commit

---
+++ MicroMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MicroMoney
+//
+//  Created by Coco Xtet Pai on 12/2/17.
+//  Copyright © 2017 Coco Xtet Pai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
