f43052cc1127e08ebd0276a031ac211073956cdf
cocoxtetpai <cocoxtetpai@gmail.com>
2017-12-15 00:15:33 +0630

    add 2 VCs in Loan Steps

--- MicroMoney/Loan Steps/EmploymentInfoVC.swift
+++ MicroMoney/Loan Steps/EmploymentInfoVC.swift
@@ -9,27 +9,30 @@
 import UIKit
 
 class EmploymentInfoVC: UIViewController {
+    
+    @IBOutlet weak var socialStatusTextField: UITextField!
+    @IBOutlet weak var companyNameTextField: UITextField!
+    @IBOutlet weak var companyNumberTextField: UITextField!
+    @IBOutlet weak var incomTextField: UITextField!
+    @IBOutlet weak var coworkerNumberTextField: UITextField!
+    @IBOutlet weak var coworkerNameTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        
+        let nextVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "GettingMoneyVC") as! GettingMoneyVC
+        
+        navigationController?.pushViewController(nextVC, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        
+        navigationController?.popViewController(animated: true)
     }
-    */
 
 }
--- MicroMoney/Loan Steps/GettingMoneyVC.swift
+++ MicroMoney/Loan Steps/GettingMoneyVC.swift
@@ -9,27 +9,30 @@
 import UIKit
 
 class GettingMoneyVC: UIViewController {
+    
+    @IBOutlet weak var totalAmmountLabel: UILabel!
+    @IBOutlet weak var repaymentDateLabel: UILabel!
+    
+    @IBOutlet weak var paymentTextField: UITextField!
+    @IBOutlet weak var accountNumberTextField: UITextField!
+    @IBOutlet weak var iDTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        
+        let nextVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DocumentScanVC") as! DocumentScanVC
+        
+        navigationController?.pushViewController(nextVC, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        
+        navigationController?.popViewController(animated: true)
     }
-    */
 
 }
